c1dafca22703ca1e6f667a8a9d78ec98fcfbdc83
Bryan Finlayson <bdfinlayson@gmail.com>
2015-05-05 20:12:26 -0500

    add integration tests. Fixed spacing and alignment in files

--- tests/integration/test_integration_cheers.rb
+++ tests/integration/test_integration_cheers.rb
@@ -1,36 +1,83 @@
-require_relative 'test_helper'
+require_relative '../test_helper'
 
-# require 'bundler/setup'
-
-class TestCheersIntegration < Minitest::Test
-  def test_no_input
+class TestCheers < Minitest::Test
+  def test_happy_path
     shell_output = ''
     expected = ''
     IO.popen('./cheers', 'r+') do |pipe|
-      expected << "Hello! What is your name?\n"
-      pipe.puts 'Bry'
-      expected << "Give me a... B!\n"
-      expected << "Give me an... R!\n"
-      expected << "Give me a... Y!\n"
-      expected << "BRY is just GRAND!\n"
-      expected << 'Nice to meet you, BRY! Now, what is your birthday?'
+      expected << "Hello, what's your name?\n"
+      pipe.puts 'Ed'
+      expected << "Give me an.. E!\n"
+      expected << "Give me a... D!\n"
+      expected << "Ed's just GRAND!\n"
+      expected << "Hey Ed, what's your birthday? (mm/dd)\n"
       pipe.puts '05/06'
-      expected << "\nAwesome! Your birthday is in 1 day! Happy Birthday in advance!"
+      expected << "Awesome! Your birthday is in 1 day! Happy Birthday in advance!\n"
       pipe.close_write
       shell_output = pipe.read
     end
     assert_equal expected, shell_output
   end
 
-  def test_happy_path_with_birthday_in_future
-    fail
+  def test_happy_path2
+    shell_output = ''
+    expected = ''
+    IO.popen('./cheers', 'r+') do |pipe|
+      expected << "Hello, what's your name?\n"
+      pipe.puts 'Bo'
+      expected << "Give me a... B!\n"
+      expected << "Give me an.. O!\n"
+      expected << "Bo's just GRAND!\n"
+      expected << "Hey Bo, what's your birthday? (mm/dd)\n"
+      pipe.puts '05/04'
+      expected << "Awesome! Your birthday is in 364 days! Happy Birthday in advance!\n"
+      pipe.close_write
+      shell_output = pipe.read
+    end
+    assert_equal expected, shell_output
   end
 
-  def test_happy_path_with_birthday_today
-    fail
+  def test_no_input
+    shell_output = ''
+    expected = ''
+    IO.popen('./cheers', 'r+') do |pipe|
+      expected << "Hello, what's your name?\n"
+      pipe.puts ''
+      expected << "I'm sorry, what was your name again?"
+      pipe.puts ''
+      expected << "I'm sorry, what was your name again?"
+      pipe.puts 'Ed'
+      expected << "Give me an.. E!\n"
+      expected << "Give me a... D!\n"
+      expected << "Ed's just GRAND!\n"
+      expected << "Hey Ed, what's your birthday? (mm/dd)\n"
+      pipe.puts '25/05'
+      expected << "I'm sorry, I don't understand :( Try again next time.\n"
+      pipe.close_write
+      shell_output = pipe.read
+    end
+    assert_equal expected, shell_output
   end
 
-  def test_happy_path_with_birthday_in_past
-    fail
+  def test_bad_birthday_format
+    shell_output = ''
+    expected = ''
+    IO.popen('./cheers', 'r+') do |pipe|
+      expected << "Hello, what's your name?\n"
+      pipe.puts 'Ed'
+      expected << "Give me an.. E!\n"
+      expected << "Give me a... D!\n"
+      expected << "Ed's just GRAND!\n"
+      expected << "Hey Ed, what's your birthday? (mm/dd)\n"
+      pipe.puts '25/05'
+      expected << "I couldn't understand that. Could you give that to me in mm/dd format?\n"
+      pipe.puts '12/2001'
+      expected << "I couldn't understand that. Could you give that to me in mm/dd format?\n"
+      pipe.puts '05/25'
+      expected << "Awesome! Your birthday is in 20 days! Happy Birthday in advance!\n"
+      pipe.close_write
+      shell_output = pipe.read
+    end
+    assert_equal expected, shell_output
   end
 end
--- tests/unit/test_unit_name_parser.rb
+++ tests/unit/test_unit_name_parser.rb
@@ -2,27 +2,27 @@ require_relative '../test_helper'
 require_relative '../../lib/cheer'
 
 class TestNameParser < Minitest::Test
-
-def test_unit_0a_raise_error_for_name_thats_blank
-  assert_raises(ArgumentError) do
-    Cheer.for_person("")
+  def test_unit_0a_raise_error_for_name_thats_blank
+    assert_raises(ArgumentError) do
+      Cheer.for_person('')
+    end
   end
-end
 
-def test_unit_0b_raise_error_for_name_thats_all_whitespace
-  assert_raises(ArgumentError) do
-    Cheer.for_person("  ")
+  def test_unit_0b_raise_error_for_name_thats_all_whitespace
+    assert_raises(ArgumentError) do
+      Cheer.for_person('  ')
+    end
   end
-end
 
-def test_unit_0c_raise_error_for_name_that_has_no_word_characters
-  assert_raises(ArgumentError) do
-    Cheer.for_person("*!?")
+  def test_unit_0c_raise_error_for_name_that_has_no_word_characters
+    assert_raises(ArgumentError) do
+      Cheer.for_person('*!?')
+    end
   end
-end
 
-def test_unit_0d_raise_error_for_birthday_instead_of_name
-  assert_raises(ArgumentError) do
-    Cheer.for_person("08/25")
+  def test_unit_0d_raise_error_for_birthday_instead_of_name
+    assert_raises(ArgumentError) do
+      Cheer.for_person('08/25')
+    end
   end
 end
